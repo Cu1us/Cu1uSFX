c75913c841f0c0db4e77eacc94abd573c128378e
Måns Fritiofsson <mans@fritiofson.net>
2026-03-15 18:59:37 +0100

    Add audiosource pool prewarming functionality

--- Runtime/SFXPlayer.cs
+++ Runtime/SFXPlayer.cs
@@ -300,6 +300,21 @@ namespace Cu1uSFX
                 defaultCapacity: SFXList.Instance.AudioSourcePoolDefault,
                 maxSize: SFXList.Instance.AudioSourcePoolMax
             );
+
+            // Prewarm pool
+            int prewarmAmount = SFXList.Instance.AudioSourcePoolDefault;
+            if (prewarmAmount > 0)
+            {
+                AudioSource[] buffer = new AudioSource[prewarmAmount];
+                for (int i = 0; i < prewarmAmount; i++)
+                {
+                    buffer[i] = SourcePool.Get();
+                }
+                for (int i = 0; i < prewarmAmount; i++)
+                {
+                    SourcePool.Release(buffer[i]);
+                }
+            }
         }
         static AudioSource PoolCreate()
         {
