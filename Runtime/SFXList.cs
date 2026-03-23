3bef36b09430b4b2ea4919c219459e861f6af652
Måns Fritiofsson <mans@fritiofson.net>
2026-03-23 20:11:00 +0100

    Update package version

--- Runtime/SFXList.cs
+++ Runtime/SFXList.cs
@@ -29,7 +29,7 @@ namespace Cu1uSFX.Internal
     public class SFXList : ScriptableObject
     {
         public const string SINGLETON_ASSET_NAME = "SFX List";
-        public const string PLUGIN_VERSION = "1.4.1";
+        public const string PLUGIN_VERSION = "1.4.2";
 
         static SFXList _instance;
         /// <summary>
